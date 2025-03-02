4d9d2c3c0528738d81bd8beb3e2a0cb34ac05a09
Giorgi Gamgebeli <166652582+Giorgi-Gamgebeli@users.noreply.github.com>
2025-03-02 15:01:45 +0400

    quick fix

--- apps/web/app/_hooks/useRedirectProgressBar.ts
+++ apps/web/app/_hooks/useRedirectProgressBar.ts
@@ -1,39 +1,45 @@
-"use client";
+// "use client";
 
-import { startProgress, stopProgress } from "next-nprogress-bar";
-import { usePathname, useRouter } from "next/navigation";
+// import { startProgress, stopProgress } from "next-nprogress-bar";
+// import { usePathname, useRouter } from "next/navigation";
 
-type PassedInFunction = () => Promise<string | undefined>;
+// type PassedInFunction = () => Promise<string | undefined>;
 
-type RedirectFunctionSettings = {
-  redirectTo: string;
-  scroll?: boolean;
-};
+// type RedirectFunctionSettings = {
+//   redirectTo: string;
+//   scroll?: boolean;
+// };
+
+// function useRedirectProgressBar() {
+//   const router = useRouter();
+//   const pathname = usePathname();
+
+//   const redirectFunction = async (
+//     passedInfunction: PassedInFunction,
+//     { redirectTo, scroll = true }: RedirectFunctionSettings
+//   ) => {
+//     startProgress();
+
+//     await passedInfunction();
+
+//     router.push(redirectTo, { scroll });
+
+//     await new Promise((res) => pathname === redirectTo && res);
+
+//     stopProgress();
+
+//     return;
+//   };
+
+//   return {
+//     redirectFunction,
+//   };
+// }
+
+// export default useRedirectProgressBar;
 
 function useRedirectProgressBar() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const redirectFunction = async (
-    passedInfunction: PassedInFunction,
-    { redirectTo, scroll = true }: RedirectFunctionSettings
-  ) => {
-    startProgress();
-
-    await passedInfunction();
-
-    router.push(redirectTo, { scroll });
-
-    await new Promise((res) => pathname === redirectTo && res);
-
-    stopProgress();
-
-    return;
-  };
-
-  return {
-    redirectFunction,
-  };
+  return;
 }
 
 export default useRedirectProgressBar;
